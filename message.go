e36dfc11c9a585412cb478f18cd485cc9867ce1c
Frol Fomich <frol.fomich@gmail.com>
2021-07-08 13:32:57 -0700

    pub/sub interfaces moved to message.go

--- message.go
+++ message.go
@@ -19,3 +19,19 @@ type Message interface {
 	//Data message payload
 	Data() interface{}
 }
+
+//Subscriber - messages receiver
+type Subscriber interface {
+	//OnMessage - invoked when message come to the input
+	//  topic - topic from which message come
+	//  m - message
+	OnMessage(topic string, m Message) bool
+}
+
+//Publisher - producer of messages
+type Publisher interface {
+	//Subscribe - adds new subscriber to topic
+	//  topic - interested topic
+	//  s - subscriber which will be invoked on new message in topic
+	Subscribe(topic string, s Subscriber)
+}
